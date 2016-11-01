dd705ba84e1dd12cb2cb648edb1e28f291b4c29a
kispygr <kispygr@gmail.com>
2016-11-01 11:52:36 +0000

    Space Invaders 1st commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float speed;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if( Input.GetKeyDown( KeyCode.Space ) )
+			
+		/* Movement */
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 position = this.transform.position;
+			position.x += speed;
+			this.transform.position = position;
+
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow)) {
+			Vector3 position = this.transform.position;
+			position.x -= speed;
+			this.transform.position = position;
+		}
+	}
+}
---
+++ Assets/ShotBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehaviour : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		position.y += speed;
+		this.transform.position = position;
+	}
+}
